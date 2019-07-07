e04fc6285a5e9f6a7cf2ac539de0094b671cf5dd
Mary West <westmarye1219@gmail.com>
2019-07-07 17:12:02 -0500

    added some content on the home page

--- src/javascripts/helpers/events.js
+++ src/javascripts/helpers/events.js
@@ -1,10 +1,18 @@
 import projects from '../projects/projects';
 
 const addEvents = () => {
+  document.getElementById('navToHome').addEventListener('click', (e) => {
+    e.preventDefault();
+    document.getElementById('technologiesPage').classList.add('hideStuff');
+    document.getElementById('projectsPage').classList.add('hideStuff');
+    document.getElementById('bioPage').classList.add('hideStuff');
+    document.getElementById('homePage').classList.remove('hideStuff');
+  });
   document.getElementById('navToBio').addEventListener('click', (e) => {
     e.preventDefault();
     document.getElementById('technologiesPage').classList.add('hideStuff');
     document.getElementById('projectsPage').classList.add('hideStuff');
+    document.getElementById('homePage').classList.add('hideStuff');
     document.getElementById('bioPage').classList.remove('hideStuff');
   });
   document.getElementById('navToProjects').addEventListener('click', (e) => {
@@ -12,6 +20,7 @@ const addEvents = () => {
     document.getElementById('technologiesPage').classList.add('hideStuff');
     document.getElementById('projectsPage').classList.remove('hideStuff');
     document.getElementById('bioPage').classList.add('hideStuff');
+    document.getElementById('homePage').classList.add('hideStuff');
   });
   document.getElementById('navToTechnologies').addEventListener('click', (e) => {
     projects.createProjectCards();
@@ -19,6 +28,7 @@ const addEvents = () => {
     document.getElementById('bioPage').classList.add('hideStuff');
     document.getElementById('projectsPage').classList.add('hideStuff');
     document.getElementById('technologiesPage').classList.remove('hideStuff');
+    document.getElementById('homePage').classList.add('hideStuff');
   });
 };
 
